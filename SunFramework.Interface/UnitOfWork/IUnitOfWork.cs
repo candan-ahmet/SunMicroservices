207e96d8854fb30849463c4518047203ab42c37c
candan-ahmet <candan-ahmet@outlook.com>
2020-01-18 00:59:34 +0300

    System.Data dll i eklendi

--- SunFramework.Interface/UnitOfWork/IUnitOfWork.cs
+++ SunFramework.Interface/UnitOfWork/IUnitOfWork.cs
@@ -3,6 +3,7 @@ using SunFramework.Interface.Model;
 using SunFramework.Interface.Repository;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
